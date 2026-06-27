9ad3ad3ca143a3f080c83437cf185187202998ba
Emmanuel-crt-I <damilareosun@gmail.com>
2026-06-28 00:15:22 +0100

    Wrap login page in Suspense

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import { AuthPanels } from '@/components/auth-panels'
 
 export default function LoginPage() {
-  return <AuthPanels />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <AuthPanels />
+    </Suspense>
+  )
 }
